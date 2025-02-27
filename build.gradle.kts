f698e7336d969b3dadc2bb82b2f53f2de62fa07d
WolfyScript <wolfyscript@gmail.com>
2025-02-27 14:26:33 +0100

    Use proper env variable names

--- build.gradle.kts
+++ build.gradle.kts
@@ -34,8 +34,8 @@ artifactory {
         contextUrl = "https://artifacts.wolfyscript.com/artifactory"
         repository {
             repoKey = "gradle-dev-local"
-            username = project.properties["wolfyRepoPublishUsername"].toString()
-            password = project.properties["wolfyRepoPublishToken"].toString()
+            username = project.properties["ARTIFACTORY_USERNAME"].toString()
+            password = project.properties["ARTIFACTORY_TOKEN"].toString()
         }
         defaults {
             publications(ALL_PUBLICATIONS)
