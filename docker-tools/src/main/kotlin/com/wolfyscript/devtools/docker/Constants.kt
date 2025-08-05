80f17947edd0438ff759fb278a5684dd14823b4f
WolfyScript <wolfyscript@gmail.com>
2025-08-05 20:02:09 +0200

    impl: add constants for ext and task names

---
+++ docker-tools/src/main/kotlin/com/wolfyscript/devtools/docker/Constants.kt
@@ -0,0 +1,18 @@
+package com.wolfyscript.devtools.docker
+
+object Constants {
+
+    // Docker Run
+    const val TASK_DOCKER_RUN_GROUP_NAME = "docker-run"
+    const val TASK_DOCKER_RUN_NAME = "dockerRun"
+    const val TASK_DOCKER_STOP_NAME = "dockerStop"
+    const val TASK_DOCKER_STATUS_NAME = "dockerStatus"
+    const val EXT_DOCKER_RUN_NAME = "dockerRun"
+
+    // Minecraft Run
+    const val TASK_MINECRAFT_RUN_GROUP_NAME = "minecraft-run"
+    const val EXT_MINECRAFT_RUN_NAME = "minecraftDockerRun"
+    const val EXT_MINECRAFT_SERVERS_NAME = "minecraftServers"
+
+
+}
